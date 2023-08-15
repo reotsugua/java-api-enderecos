d9dc3bf8806894c68d1132416a61d1db5fa57a4e
reotsugua <renanaugusto.ras@gmail.com>
2023-08-15 01:02:01 -0300

    upando springboot

---
+++ logradouros/src/main/java/com/projeto/logradouros/LogradourosApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.logradouros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogradourosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogradourosApplication.class, args);
+	}
+
+}
---
+++ logradouros/src/test/java/com/projeto/logradouros/LogradourosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.logradouros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogradourosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
