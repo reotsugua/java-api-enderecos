096612a9b39668e030c5851816f9cb1224ded8da
reotsugua <renanaugusto.ras@gmail.com>
2023-08-16 23:44:12 -0300

    corrigindo a service

--- logradouros/src/main/java/com/projeto/logradouros/service/ViaCepService.java
+++ logradouros/src/main/java/com/projeto/logradouros/service/ViaCepService.java
@@ -1,12 +1,24 @@
 package com.projeto.logradouros.service;
 
+import com.projeto.logradouros.exception.EnderecoNotFoundException;
 import com.projeto.logradouros.model.Endereco;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ViaCepService {
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Endereco buscarEnderecoPorCep(String cep) throws EnderecoNotFoundException {
-        // Implemente a lógica para fazer a chamada à API do ViaCEP
-        // e retornar os dados do endereço. Trate os casos de erro conforme necessário.
+        String viaCepUrl = "https://viacep.com.br/ws/" + cep + "/json/";
+        Endereco endereco = restTemplate.getForObject(viaCepUrl, Endereco.class);
+
+        if (endereco == null || endereco.getCep() == null) {
+            throw new EnderecoNotFoundException("Endereço não encontrado para o CEP fornecido.");
+        }
+
+        return endereco;
     }
 }
